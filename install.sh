4f376f6d38dc594f8bc6b591d6fbc9ad429fdd34
Sunny Patel <du.svp28@gmail.com>
2013-09-04 15:11:36 -0400

    tiny changes

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not install
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 #git clone https://github.com/gmarik/vundle.git ~/testVimPack
 
-echo "Do you want to overwrite your .vimrc?"
+echo "Do you want to overwrite your .vimrc? [y/n]"
 read overwrite
 
 if [ "$overwrite" == "y" ] || [ "$overwrite" == "yes" ] || [ "$overwrite" == "" ]; then
 	echo "overwriting .vimrc"
 	cp vimrc ~/.vimrc
 
-	echo "Run vim and enter :BundleInstall command, this will install all plugins"
+	echo "Run vim and enter :BundleInstall command, this will install all plugins."
 else
 	echo "skipping .vimrc"
 fi
