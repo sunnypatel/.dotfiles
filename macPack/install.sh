6feadb336e6b07f2b5be418fffcbb98d2806f9f8
Sunny Patel <du.svp28@gmail.com>
2023-11-18 13:41:35 -0600

    feat(macPack): sets up mac w/ dev essentials

--- macPack/install.sh
+++ macPack/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Install node via nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/cask
 
 # Install tree
 brew install tree
@@ -11,14 +15,24 @@ brew install nvim
 # Install NvChad
 # git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
-# Install LunarNvim
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
-
+# --- LunarVim prerequisites start
 
 # Install LazyGit
 brew install lazygit
 
+# Install Rust 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Pip
+python get-pip.py
+
 # Install Mononoki Nerd font
-mkdir -p ./.tmp_macpack
-curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Mononoki.zip ./.tmp_macpack/nerd-fonts/
-unzip ./.tmp_macpack/nerd-fonts/Mononoki.zip
+brew tap homebrew/cask-fonts
+brew cask install font-mononoki-nerd-font
+
+# --- LunarVim prerequisites end
+
+# Install LunarNvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+
