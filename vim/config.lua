ce3b06b8ffe55ce9afa9961ffd9496e85f47dddc
Sunny Patel <spatel2@paypal.com>
2023-12-06 21:47:02 -0600

    adds flutter support

--- vim/config.lua
+++ vim/config.lua
@@ -14,7 +14,100 @@ lvim.plugins = {
     lazy = false,
     priority = 1000,
     opts = {},
-  }
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    requires = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    config = function()
+      require('flutter-tools').setup {
+        -- flutter_path = "~/development/flutter",
+        fvm = true, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
+        ui = {
+          -- the border type to use for all floating windows, the same options/formats
+          -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+          border = "rounded",
+          -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+          -- please note that this option is eventually going to be deprecated and users will need to
+          -- depend on plugins like `nvim-notify` instead.
+          notification_style = "plugin",
+        },
+        decorations = {
+          statusline = {
+            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+            -- this will show the current version of the flutter app from the pubspec.yaml file
+            app_version = true,
+            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+            -- this will show the currently running device if an application was started with a specific
+            -- device
+            device = true,
+          },
+        },
+        outline = {
+          open_cmd = "30vnew", -- command to use to open the outline buffer
+          auto_open = false,   -- if true this will open the outline automatically when it is first populated
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          register_configurations = function(_)
+            local dap = require("dap")
+            -- dap.adapters.dart = {
+            -- 	type = "executable",
+            -- 	command = "node",
+            -- 	args = { debugger_path, "flutter" },
+            -- }
+            dap.set_log_level("TRACE")
+            dap.configurations.dart = {}
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          enabled = false,
+          -- open_cmd = "tabedit", -- command to use to open the log buffer
+        },
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = false, -- highlight the background
+            foreground = false, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = "■", -- the virtual text character to highlight
+          },
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+            renameFilesWithClasses = "prompt", -- "always"
+            enableSnippets = true,
+            enableSdkFormatter = true,
+          },
+        },
+
+      }
+    end
+  },
+  -- Syntax highlight for mdx files: used by Storybook
+  { "jxnblk/vim-mdx-js" },
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      require("tmux").setup {
+        navigation = {
+          -- cycles to opposite pane while navigating into the border
+          -- cycle_navigation = true,
+
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+
+          -- prevents unzoom tmux when navigating beyond vim border
+          persist_zoom = true,
+        },
+        resize = {
+          -- enables default keybindings (A-hjkl) for normal mode
+          enable_default_keybindings = true,
+        },
+      }
+    end,
+  },
 }
 
 -- colorizer plugin for color highlighting
@@ -46,7 +139,7 @@ formatters.setup {
     ---@usage arguments to pass to the formatter
     -- these cannot contain whitespace
     ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
-    filetypes = { "typescript", "typescriptreact" },
+    filetypes = { "typescript", "typescriptreact", 'javascript', 'javascriptreact' },
   },
 }
 
@@ -66,8 +159,30 @@ code_actions.setup {
   },
 }
 
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "typescript",
+  "tsx",
+  "css",
+  "yaml",
+  "dart",
+  "ruby",
+  "markdown",
+  "markdown_inline"
+}
+
+
 -- Format on save
 lvim.format_on_save.enabled = true
 
 -- Auto change to current directory
 vim.opt.autochdir = true
+
+-- Flutter snippets enable
+local luasnip = require("luasnip")
+luasnip.filetype_extend("dart", { "flutter" })
